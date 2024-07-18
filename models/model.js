656e860eafc97e5de500ab5e3adafa5e272d360e
Kyle Compton <kylecompton@hotmail.co.uk>
2024-07-18 11:15:22 +0100

    amendments made based on mitchs comment

--- models/model.js
+++ models/model.js
@@ -9,12 +9,15 @@ function fetchTopics() {
 
 function fetchArticleById(article_id) {
   return db
-    .query(`SELECT articles.* ,
+    .query(
+      `SELECT articles.* ,
     COUNT(comments.comment_id) AS comment_count 
     FROM articles 
     LEFT JOIN comments ON articles.article_id = comments.article_id
     WHERE articles.article_id = $1
-    GROUP BY articles.article_id`,[article_id])
+    GROUP BY articles.article_id`,
+      [article_id]
+    )
     .then((article) => {
       if (article.rows.length === 0) {
         return Promise.reject({
@@ -39,8 +42,6 @@ function fetchArticles(sort_by = "created_at", order_by = "DESC", topic) {
 
   const validOrderBys = [`ASC`, `DESC`];
 
-  const validTopics = ["mitch","cats","paper"]
-
   if (!validSortBys.includes(sort_by)) {
     sort_by = "created_at";
   }
@@ -49,29 +50,40 @@ function fetchArticles(sort_by = "created_at", order_by = "DESC", topic) {
     order_by = "DESC";
   }
 
-  if(!validTopics.includes(topic)){
-    topic = ""
-  }
-
   let sqlString = `SELECT articles.article_id, title, topic, articles.author, articles.created_at, articles.votes, article_img_url, 
   COUNT(comment_id) AS comment_count 
   FROM articles
   LEFT JOIN comments ON articles.article_id = comments.article_id`;
 
   const queryValues = [];
-
   if (topic) {
-    sqlString += ` WHERE topic = $1`;
-    queryValues.push(topic);
+    return db
+      .query(`SELECT * FROM topics WHERE slug = $1`, [topic])
+      .then((result) => {
+        if (result.rows.length === 1) {
+          sqlString += ` WHERE topic = $1`;
+          queryValues.push(topic);
+        } else {
+          return Promise.reject({
+            status: 404,
+            message: "Topic does not exist",
+          });
+        }
+
+        sqlString += ` GROUP BY articles.article_id ORDER BY ${sort_by} ${order_by}`;
+
+        return db.query(sqlString, queryValues).then((articles) => {
+          return articles.rows;
+        });
+      });
+  } else {
+    sqlString += ` GROUP BY articles.article_id ORDER BY ${sort_by} ${order_by}`;
+
+    return db.query(sqlString).then((articles) => {
+      return articles.rows;
+    });
   }
-
-  sqlString += ` GROUP BY articles.article_id ORDER BY ${sort_by} ${order_by}`;
-
-  return db.query(sqlString, queryValues).then((articles) => {
-    return articles.rows;
-  });
 }
-
 function fetchArticleComments(article_id) {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
