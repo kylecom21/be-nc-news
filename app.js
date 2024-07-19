b9157a05a9960f0e7958bf805e118ce00a54844f
Kyle Compton <kylecompton@hotmail.co.uk>
2024-07-19 16:06:10 +0100

    routing complete

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
 const express = require("express");
 const app = express();
-const {
-  getTopics,
-  getEndpoints,
-  getArticleById,
-  getArticles,
-  getArticleComments,
-  createArticleComment,
-  patchArticleVotes,
-  deleteCommentByID,
-  getUsers,
-} = require("./controllers/controller");
+const routeIndex = require('./routes');
+
 
 app.use(express.json());
 
-app.get("/api", getEndpoints);
+app.use('/api', routeIndex);
 
-app.get("/api/topics", getTopics);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id/comments", getArticleComments);
-
-app.post("/api/articles/:article_id/comments", createArticleComment)
-
-app.patch("/api/articles/:article_id", patchArticleVotes )
-
-app.delete("/api/comments/:comment_id", deleteCommentByID)
-
-app.get("/api/users", getUsers)
 
 app.use((err, request, response, next) => {
   if (err.code === "22P02" ||err.code === '23502'|| err.code === "23503" ) {
